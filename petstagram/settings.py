8c8d256c17e0296f3b34306f78d11273b0cf59df
ven10 <ven.hristov@gmail.com>
2024-09-25 09:18:48 +0300

    cng: staticfiles -> static

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'staticfiles/',
+    BASE_DIR / 'static/',
 )
 
 # Default primary key field type
