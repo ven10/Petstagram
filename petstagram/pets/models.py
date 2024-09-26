92eb5e54bd77656f18a759a49e72aa5eb713971b
ven10 <ven.hristov@gmail.com>
2024-09-26 09:52:57 +0300

    add: pets model

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+
+    personal_photo = models.URLField()
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)    # to have an ID value
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        super().save(*args, **kwargs)
--- petstagram/settings.py
+++ petstagram/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q3+ot^1+rl6skv*$dxzf(ym9k53b$n8p!#css!01so1us#-c7u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
