c4583b5987882b58c2b0c34ed3fcfa4a0c851611
ven10 <ven.hristov@gmail.com>
2024-09-26 13:30:46 +0300

    feat: added conmments and like models

--- petstagram/common/models.py
+++ petstagram/common/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from petstagram.photos.models import Photo
+
+
 # Create your models here.
+
+class Comment(models.Model):
+    text = models.TextField(
+        max_length=300,
+        help_text='Comment must not exceed 300 characters.'
+    )
+
+    date_time_of_publication = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Publication date and time'
+    )
+    to_photo = models.ForeignKey(
+        to=Photo,
+        on_delete=models.CASCADE,
+    )
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(
+        to=Photo,
+        on_delete=models.CASCADE,
+    )
