5f5ae8790a63060142cc15757d394f27e5f39bfe
ven10 <ven.hristov@gmail.com>
2024-09-26 13:22:15 +0300

    fix:photo model admin panel

--- petstagram/photos/admin.py
+++ petstagram/photos/admin.py
@@ -5,5 +5,10 @@ from petstagram.photos.models import Photo
 # Register your models here.
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'date_of_publication', 'description', 'get_tagged_pets')
 
+    @staticmethod
+    # def get_tagged_pets(obj):
+    #     return ', '.join([pet.name for pet in obj.tagged_pets.all()])
+    def get_tagged_pets(obj):
+        return ', '.join(str(pet) for pet in obj.tagged_pets.all())
