b3fa1c13bb4794871070c4b235d502615864d21b
Венелин Христов <v.hristov@rg-plovdiv.dep>
2024-10-10 15:25:29 +0300

    end: ended part 2.1

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -3,7 +3,7 @@ from petstagram.accounts import views
 
 urlpatterns =(
     path('login/', views.login, name="login"),
-    path('register/', views.register, name="login"),
+    path('register/', views.register, name="register"),
     path('profile/<int:pk>/', include([
         path('', views.profile_details, name="profile-details"),
         path('edit/', views.profile_edit, name="profile-edit"),
--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from petstagram.photos import views
 
 urlpatterns = [
-    path('add/', views.photo_add_page, name='photo-add'),
+    path('add/', views.photo_add_page, name='add-photo'),
     path('<int:pk>/', include([
-        path('', views.photo_details_page, name='photo-details'),
-        path('edit/', views.photo_edit_page, name='photo-edit'),
+        path('', views.photo_details_page, name='details-photo'),
+        path('edit/', views.photo_edit_page, name='edit-photo'),
         ]),
     )
 ]
